878181f66611a346854d7b97679541159b21916e
dung2410-art <ntd113663@gmail.com>
2022-04-27 17:35:47 +0700

    gff
    user

---
+++ src/main/java/com/dung/mobileshop/controllers/AdminController.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.dung.mobileshop.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Dung
+ */
+@WebServlet(name="AdminController",
+            urlPatterns = {"/admin",
+                           "/admin/products",
+                           "/admin/transactions",
+                           "/admin/user"})
+public class AdminController extends HttpServlet {    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session=request.getSession();    
+        if(session.getAttribute("userrole")==null || !session.getAttribute("userrole").toString().equals("1")){
+            response.sendRedirect("/mobileshop/login");
+            return;
+        }        
+        RequestDispatcher requestDispatcher = null;
+        String userPath = request.getServletPath();
+        if (userPath.equals("/admin/products")) {
+            // TODO: Implement 
+        }else  if (userPath.equals("/admin")) {            
+            requestDispatcher = getServletContext().getRequestDispatcher("/index-admin.jsp");
+        }
+        requestDispatcher.forward(request,response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
